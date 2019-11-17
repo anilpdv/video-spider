72b56a1f331b0be01e596958d85487bbbe66ba31
anilpdv <pdvanil007@gmail.com>
2019-11-17 23:09:09 +0530

    fixing error

--- commands.js
+++ commands.js
@@ -3,12 +3,12 @@ const program = require('commander');
 const chalk = require('chalk');
 const {get_movie} = require('./app');
 
-program.version('1.0.0').description('video-spider');
+program.version('1.0.1').description('video-spider');
 
 // : generates random quote
 program
-  .command('id <videoId>')
-  .alias('-i')
+  .command('imdbid <videoId>')
+  .alias('id')
   .description(chalk.blue('gets the movie link '))
   .action(videoId => {
     get_movie(videoId);
